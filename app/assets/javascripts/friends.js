6344e19e49fae6bd46950272716b42a12268c48c
venkatasubbaiah <venky.kandagaddala@gmail.com>
2013-05-10 12:43:46 +0530

    Enable autoscroll for friends index page only

--- app/assets/javascripts/friends.js
+++ app/assets/javascripts/friends.js
@@ -1,21 +1,26 @@
 $(function () {
 
+    if(params["action"] == "index" && params["controller"]== "friends") {
+
     $(window).scroll(function (e) {
-        console.log('friends index');
-        e.preventDefault();
-        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-            var next_page_no = $('#next_page_no').val();
-            var $next = parseInt(next_page_no);
-            var total_no_pages = $('#total_no_pages').val();
-            var $total = parseInt(total_no_pages);
+        var next_page_no = $('#next_page_no').val();
+        var $next = parseInt(next_page_no);
+        var total_no_pages = $('#total_no_pages').val();
+        var $total = parseInt(total_no_pages);
+        if ($total > 1) {
+            e.preventDefault();
+            if ($(window).scrollTop() + $(window).height() == $(document).height()) {
 //            alert('next = ' + $n + 'total = ' + $t)
-            if ($next < $total) {
-                $.get('/friends', {page: next_page_no},
-                    function () {
-                    });
+                if ($next < $total) {
+                    $.get('/friends', {page: next_page_no},
+                        function () {
+                        });
+                }
             }
         }
+
     });
+    }
 })
 ;
 
--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,7 @@
 class FriendsController < ApplicationController
 
   def index
+
     @friends = current_user.friends.paginate(:per_page => 10, :page => params[:page])
     respond_to do |format|
       format.js
